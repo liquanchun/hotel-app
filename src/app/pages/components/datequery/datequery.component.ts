57a19b037b36c908d991dd49dfc5ef36376bbfa9
APAC\listev <350966707@qq.com>
2017-12-10 22:23:18 +0800

    update

--- src/app/pages/components/datequery/datequery.component.ts
+++ src/app/pages/components/datequery/datequery.component.ts
@@ -8,9 +8,7 @@ import * as _ from 'lodash';
     template: `
     <div class="btn-group margin8 pull-right" role="group" aria-label="Basic example">
     <button (click)="onClick($event)" type="button" class="btn btn-info btn-sm"><i class="fa fa-repeat" aria-hidden="true"></i> 全部</button>
-    <button (click)="onClick($event)" type="button" class="btn btn-info btn-sm"><i class="fa fa-angle-left" aria-hidden="true"></i> 昨天</button>
     <button (click)="onClick($event)" type="button" class="btn btn-info btn-sm"><i class="fa fa-angle-down" aria-hidden="true"></i> 今天</button>
-    <button (click)="onClick($event)" type="button" class="btn btn-info btn-sm"><i class="fa fa-angle-double-left" aria-hidden="true"></i> 上周</button>
     <button (click)="onClick($event)" type="button" class="btn btn-info btn-sm"><i class="fa fa-angle-double-down" aria-hidden="true"></i> 本周</button>
     <button (click)="onClick($event)" type="button" class="btn btn-info btn-sm"><i class="fa fa-angle-double-right" aria-hidden="true"></i> 本月</button>
     </div>
